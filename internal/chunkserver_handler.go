115af8e9e22ba8966dcbf2be8050f19b913fb647
zzhtttsss <634879238@qq.com>
2022-11-07 19:51:20 +0800

    feat(log): use logger to log to file.

--- internal/chunkserver_handler.go
+++ internal/chunkserver_handler.go
@@ -8,16 +8,17 @@ import (
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/peer"
 	"google.golang.org/grpc/status"
-	"log"
 	"net"
 	"os"
 	"time"
 	"tinydfs-base/common"
+	"tinydfs-base/config"
 	"tinydfs-base/protocol/pb"
 	"tinydfs-base/util"
 )
 
 var GlobalChunkServerHandler *ChunkServerHandler
+var Logger *logrus.Logger
 
 // ChunkServerHandler represent a chunkserver node to handle all incoming requests.
 type ChunkServerHandler struct {
@@ -29,13 +30,15 @@ type ChunkServerHandler struct {
 
 func CreateGlobalChunkServerHandler() {
 	var err error
+	Logger = config.InitLogger(Logger, true)
+	Logger.SetLevel(logrus.Level(viper.GetInt(common.MasterLogLevel)))
 	GlobalChunkServerHandler = &ChunkServerHandler{}
 	GlobalChunkServerHandler.EtcdClient, err = clientv3.New(clientv3.Config{
 		Endpoints:   []string{viper.GetString(common.EtcdEndPoint)},
 		DialTimeout: 5 * time.Second,
 	})
 	if err != nil {
-		logrus.Panicf("Fail to get etcd client, error detail : %s", err.Error())
+		Logger.Panicf("Fail to get etcd client, error detail : %s", err.Error())
 	}
 }
 
@@ -45,11 +48,11 @@ func CreateGlobalChunkServerHandler() {
 func (handler *ChunkServerHandler) TransferChunk(stream pb.PipLineService_TransferChunkServer) error {
 	p, _ := peer.FromContext(stream.Context())
 	address := p.Addr.String()
-	logrus.Infof("start to receive snd send chunk from: %s", address)
+	Logger.Infof("Start to receive snd send chunk from: %s", address)
 	err := DoTransferFile(stream)
 	if err != nil {
-		logrus.Errorf("Fail to check path and filename for add operation, error code: %v, error detail: %s,", common.MasterCheckArgs4AddFailed, err.Error())
-		details, _ := status.New(codes.Unavailable, err.Error()).WithDetails(&pb.RPCError{
+		Logger.Errorf("Fail to receive snd send chunk from: %s", address)
+		details, _ := status.New(codes.Internal, err.Error()).WithDetails(&pb.RPCError{
 			Code: common.ChunkServerTransferChunkFailed,
 			Msg:  err.Error(),
 		})
@@ -59,11 +62,11 @@ func (handler *ChunkServerHandler) TransferChunk(stream pb.PipLineService_Transf
 }
 
 func (handler *ChunkServerHandler) SetupStream2DataNode(args *pb.SetupStream2DataNodeArgs, stream pb.SetupStream_SetupStream2DataNodeServer) error {
-	logrus.Infof("Get request for set up stream with data node, DataNodeId: %s, ChunkId: %s", args.DataNodeId, args.ChunkId)
+	Logger.Infof("Get request for setting up stream with data node, DataNodeId: %s, ChunkId: %s", args.DataNodeId, args.ChunkId)
 	err := DoSendStream2Client(args, stream)
 	if err != nil {
-		logrus.Errorf("Fail to send stream to client for get operation, error code: %v, error detail: %s,", common.MasterCheckArgs4AddFailed, err.Error())
-		details, _ := status.New(codes.Unavailable, err.Error()).WithDetails(&pb.RPCError{
+		Logger.Errorf("Fail to send stream to client for get operation, error code: %v, error detail: %s,", common.MasterCheckArgs4AddFailed, err.Error())
+		details, _ := status.New(codes.Internal, err.Error()).WithDetails(&pb.RPCError{
 			Code: common.ChunkServerTransferChunkFailed,
 			Msg:  err.Error(),
 		})
@@ -75,16 +78,16 @@ func (handler *ChunkServerHandler) SetupStream2DataNode(args *pb.SetupStream2Dat
 func (handler *ChunkServerHandler) Server() {
 	go Heartbeat()
 	ip, _ := util.GetLocalIP()
-	logrus.Infof("local ip is: %s", ip)
+	Logger.Infof("local ip is: %s", ip)
 	listener, err := net.Listen(common.TCP, common.AddressDelimiter+viper.GetString(common.ChunkPort))
-	log.Println("Listen addr ", listener.Addr().String())
+	Logger.Infof("Listen address: %s", listener.Addr().String())
 	if err != nil {
-		logrus.Errorf("Fail to server, error code: %v, error detail: %s,", common.ChunkServerRPCServerFailed, err.Error())
+		Logger.Errorf("Fail to server, error code: %v, error detail: %s,", common.ChunkServerRPCServerFailed, err.Error())
 		os.Exit(1)
 	}
 	server := grpc.NewServer()
 	pb.RegisterPipLineServiceServer(server, handler)
 	pb.RegisterSetupStreamServer(server, handler)
-	logrus.Infof("Chunkserver is running, listen on %s%s", common.LocalIP, viper.GetString(common.ChunkPort))
+	Logger.Infof("Chunkserver is running, listen on %s%s", common.LocalIP, viper.GetString(common.ChunkPort))
 	server.Serve(listener)
 }
